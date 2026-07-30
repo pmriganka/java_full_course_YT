06972332875ae127c480db42b25f17cc4502eb96
Paul <mriganka.paul@dell.com>
2026-07-30 21:28:11 +0530

    office_30jul26

---
+++ Packages/tools/Advcalc.java
@@ -0,0 +1,11 @@
+package tools;
+
+public class Advcalc extends Calculator {
+
+    public int add(int a, int b) {
+
+        return a + b + 1;
+
+    }
+
+}
---
+++ Packages/tools/Calculator.java
@@ -0,0 +1,10 @@
+package tools;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+
+        return a + b;
+    }
+
+}
